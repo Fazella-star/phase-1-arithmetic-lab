257f377198aab804f0944622ab4e3f32681ffeda
Moringa School <edwardgitau004@gmail.com>
2023-03-18 15:04:34 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1*num2;
+console.log(multiply);
+let random = Math.floor(Math.random() *100) + 1;
+
+let num3 = 44;
+let num4 = 10;
+const mod = num3 % num4;
+console.log(mod)
+let numbers = [15, 10, 20, 8, 2];
+let max = Math.max(...numbers);
